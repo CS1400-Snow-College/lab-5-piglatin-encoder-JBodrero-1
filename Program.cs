20c163197ebdf773b90fb6cdb771b1d4e5e0e231
Jonathan Bodrero <jbodrero@hotmail.com>
2025-06-26 11:12:48 -0600

    Greeting, get message to wok on.

--- Program.cs
+++ Program.cs
@@ -2,3 +2,15 @@
 //  Lab 6 Piglatin Encoder
 //  June 26 - .., 2025
 
+
+Console.Clear();
+Console.Write("Welcome to the PigLatin encoder.  \nPlease enter your message to encrypt: ");
+
+string originalMessage = Console.ReadLine();
+Console.WriteLine(originalMessage);     //  Verify message intered correctly.
+
+string[] messageWords = originalMessage.Split(' '); // This should split message into words.
+
+
+
+
